7ced1e3a346ca1db7d70ccec6299cb14c6b1fb7e
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2023-10-22 00:53:32 -0500

    Product Presenter

--- Views/ProductView.cs
+++ Views/ProductView.cs
@@ -10,12 +10,65 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp1.Views
 {
-    public partial class ProductView : Form
+    public partial class ProductView : Form, IProductView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
+
+        public string ProductId
+        {
+            get { return TxtProductId.Text; }
+            set { TxtProductId.Text = value;}
+        }
+        string IProductView.ProductName
+        {
+            get { return TxtProductName.Text; }
+            set { TxtProductName.Text = value; }
+        }
+        public string ProductPrice
+        {
+            get { return TxtPrice.Text; }
+            set { TxtPrice.Text = value; }
+        }
+        public string ProductStock
+        {
+            get { return TxtStock.Text; }
+            set { TxtStock.Text = value;}
+        }
+        public string SearchValue
+        {
+            get { return TxtSearchS.Text; }
+            set { TxtSearchS.Text = value;}
+        }
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set { isEdit = value; }
+        }
+
+        public bool IsSuccessful
+        {
+            get { return isSuccessful; }
+            set { isSuccessful = value;}
+        }
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
         public ProductView()
         {
             InitializeComponent();
         }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
 
         private void ProductView_Load(object sender, EventArgs e)
         {
@@ -25,6 +78,11 @@ namespace Supermarket_mvp1.Views
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void SetProductListBildingSource(BindingSource productList)
+        {
+            DgProduct.DataSource = productList;
         }
     }
 }
